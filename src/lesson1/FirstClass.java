abd122a7bb1d8c74d2b235ad912437f371c4cb31
Анна Корнивец <annakornivec@MacBook-Air-Anna.local>
2019-03-06 20:11:13 +0300

    Initial commit

---
+++ src/lesson1/FirstClass.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class FirstClass {
+                                            //psvm точка входа
+    public static void main(String[] args) {
+        System.out.println("Some info");    //sout
+    }
+}
