0e5befa4864be18d1a163e67527ee40a2c3af191
annakornivec <kona.96@inbox.ru>
2019-04-19 10:39:23 +0300

    Changes

--- src/Fitness/FitnessRegistrator.java
+++ src/Fitness/FitnessRegistrator.java
@@ -83,23 +83,6 @@ public class FitnessRegistrator {
     }
 
 
-    public static void main(String[] args) throws IOException {
-        FitnessRegistrator fitnessRegistrator = new FitnessRegistrator();
-        FitnessServiceEnumeration group = FitnessServiceEnumeration.GROUP;
-        FitnessServiceEnumeration gym = FitnessServiceEnumeration.GYM;
-        FitnessServiceEnumeration pool = FitnessServiceEnumeration.POOL;
-
-        fitnessRegistrator.add(new Client("Ada", "Wong", 1968), gym);
-        fitnessRegistrator.add(new Client("Jedai", "Kein", 2008), gym);
-        fitnessRegistrator.add(new Client("Ada", "Wong", 1968), pool);
-
-        fitnessRegistrator.add(new Client("Slain", "Katar", 1968), pool);
-        fitnessRegistrator.add(new Client("Rendal", "Selk", 2008), pool);
-        fitnessRegistrator.add(new Client("Wert", "Wong", 1968), gym);
-        fitnessRegistrator.add(new Client("Salazer", "Slizeren", 2008), gym);
-
-        fitnessRegistrator.out();
-    }
 
     @Override
     public String toString() {
